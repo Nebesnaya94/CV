ed3b079db586ca20227c30d0f294de6d7f782c75
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-06-11 22:15:03 +0300

    fix: fix json link in script

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ for (let languageButton of languageButtons) {
 }
 
 function changeLanguage(lang) {
-  fetch(`../json/${lang}.json`)
+  fetch(`https://nebesnaya94.github.io/CV/json/${lang}.json`)
     .then((response) => response.json())
     .then((data) => {
       translate(title, data.titles);
